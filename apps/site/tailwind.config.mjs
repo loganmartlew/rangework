380a16a6ea915142dab5dd1ebe6f6a5a01598085
loganmartlew <logan.martlew@gmail.com>
2026-06-19 12:42:42 +1200

    Add tailwind

---
+++ apps/site/tailwind.config.mjs
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './src/**/*.{astro,html,js,jsx,md,mdx,pure,runtime,svelte,ts,tsx,vue}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
